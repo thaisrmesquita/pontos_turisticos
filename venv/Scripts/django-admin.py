32eb0a3d722ece60d8466c4ebf7dcba44c46f4ca
Thais Ribeiro <thaisribeiro.mesquita@gmail.com>
2019-01-21 11:44:44 -0300

    Configuração inicial do Projeto

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\cursos\djangorestframework\pontos_turisticos\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
