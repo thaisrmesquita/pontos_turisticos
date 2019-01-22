72b6fdcf0ab83ebb779a8c70a0f4855001ddda71
Thais Ribeiro <thaisribeiro.mesquita@gmail.com>
2019-01-22 14:33:51 -0300

    Adição da aplicação Avaliações no settings do Projeto

--- avaliacoes/models.py
+++ avaliacoes/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Avaliacao(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comentario = models.TextField(null=True, blank=True)
-    nota = models.DecimalField()
+    nota = models.DecimalField(max_digits=3, decimal_places=2)
     data = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
