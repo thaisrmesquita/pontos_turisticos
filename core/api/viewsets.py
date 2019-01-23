f5cf5fe45a059db3c88678c44dcb1b13fa7ddff8
Thais Ribeiro <thaisribeiro.mesquita@gmail.com>
2019-01-23 11:25:41 -0300

    Criação de PontoTuristicoSerializer

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,8 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
+
+from core.api.serializers import PontoTuristicoSerializer
 from core.models import PontoTuristico
 
 class AccountViewSet(ModelViewSet):
-    #pega todos os dados do banco de dados e retorna nesse objeto queryset
     queryset = PontoTuristico.objects.all()
-    #como você quer mostrar esses dados
     serializer_class = PontoTuristicoSerializer
