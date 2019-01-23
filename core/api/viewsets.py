94d333a2e57ccff6e1225e7364ccf8f04425ea8e
Thais Ribeiro <thaisribeiro.mesquita@gmail.com>
2019-01-23 11:27:13 -0300

    Correção viewset Ponto Turistico

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -3,6 +3,6 @@ from rest_framework.viewsets import ModelViewSet
 from core.api.serializers import PontoTuristicoSerializer
 from core.models import PontoTuristico
 
-class AccountViewSet(ModelViewSet):
+class PontoTuristicoViewSet(ModelViewSet):
     queryset = PontoTuristico.objects.all()
     serializer_class = PontoTuristicoSerializer
