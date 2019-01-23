b331b77d29e05c448e99fea1b031090961f93d20
Thais Ribeiro <thaisribeiro.mesquita@gmail.com>
2019-01-23 11:48:24 -0300

    Correção arquivo urls.py do projeto. Adição de path no urlpatterns

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 
@@ -22,5 +22,6 @@ router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
 
 urlpatterns = [
+    path('',include(router.urls)),
     path('admin/', admin.site.urls),
 ]
