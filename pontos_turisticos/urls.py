b38426959891f1b8577cc89d10508e9d217cee77
Thais Ribeiro <thaisribeiro.mesquita@gmail.com>
2019-01-23 11:17:06 -0300

    Criação viewset app Core

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'pontoturistico', views.UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
